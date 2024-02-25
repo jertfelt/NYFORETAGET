2fb14ff42d93bf8d612ec838c28f544ddec11bc2
Tova Jertfelt <tovajertfelt@gmail.com>
2024-02-25 17:26:44 +0100

    modal

--- modal.js
+++ modal.js
@@ -21,6 +21,147 @@ const openModal = (type) => {
         </div>
       </div>`;
       break;
+    case "registerform":
+      modalContent.innerHTML = `
+      <div class="modal" id="modalDiv">
+      <div class="modal--registerform ">
+      <button class="modal--close" onclick="closeModal()">&times;</button>
+      <div>
+                  <h1 class="h2--main form__h1">
+                    Anmäl ditt intresse till seminariet Första Stegen
+                  </h1>
+                  <form
+                    id="registerInterest"
+                    action="/submit-form"
+                    method="post"
+                    class="form--register"
+                  >
+                    <label for="contactPerson" id="contactPersonLabel">
+                      Kontaktperson:
+                    </label>
+                    <input
+                      type="text"
+                      id="contactPerson"
+                      name="contactPerson"
+                      placeholder="Ditt namn"
+                      required
+                    />
+                    <!-- Om tom visa nedan:-->
+                    <span id="contactPersonError" class="error--txt"></span>
+
+                    <fieldset>
+                      <legend id="contactMethodLabel">
+                        Välj kontaktmetod:
+                      </legend>
+                      <!-- Om ej vald, visa nedan: -->
+                      <span id="contactMethodError" class="error--txt"></span>
+                      <span class="radiospan">
+                        <input
+                          type="radio"
+                          id="phoneContact"
+                          name="contactMethod"
+                          value="phoneContact"
+                          required
+                        />
+                        <label for="phoneContact">Telefon</label>
+                      </span>
+                      <span class="radiospan">
+                        <input
+                          type="radio"
+                          id="emailContact"
+                          name="contactMethod"
+                          value="emailContact"
+                          required
+                        />
+                        <label for="emailContact">Email</label>
+                      </span>
+                    </fieldset>
+
+                    <label id="contactDetailLabel" for="contactDetail"
+                      >Telefon/Email:</label
+                    >
+                    <input
+                      type="text"
+                      id="contactDetail"
+                      name="contactDetail"
+                      placeholder="Telefonnummer/Email"
+                      required
+                    />
+                    <!-- Om fel på kontaktuppgifter visa nedan:  -->
+                    <span id="contactDetailError" class="error--txt"></span>
+
+                    <span class="formcontainer--labelandinfo">
+                      <label
+                        class="organisationInputLabel"
+                        for="organisationInput"
+                        >Organisationsnummer:</label
+                      >
+                      <p class="info">(valfritt)</p>
+                    </span>
+                    <label
+                      class="notmobile organisationInputLabel"
+                      for="organisationInput"
+                      >Organisationsnummer:</label
+                    >
+                    <span class="row--notmobile">
+                      <input
+                        type="text"
+                        id="organisationInput"
+                        name="orgNr"
+                        placeholder="10 siffror"
+                      />
+                      <p class="detail info--optional">(valfritt)</p>
+                    </span>
+                    <!-- Om fel på organisationsnummer visa nedan: -->
+                    <span id="organisationError" class="error--txt"></span>
+
+                    <span class="formcontainer__labelandinfo">
+                      <label for="companyName">Företagsnamn:</label>
+                      <p class="info">(valfritt)</p>
+                    </span>
+                    <label for="companyName" class="notmobile"
+                      >Företagsnamn:</label
+                    >
+                    <span class="row--notmobile">
+                      <input
+                        type="text"
+                        id="companyName"
+                        name="companyName"
+                        placeholder="Företagsnamn AB"
+                      />
+                      <p class="detail info--optional">(valfritt)</p>
+                    </span>
+                    <!-- Om  fel på flera ställen, visa errormeddelande nedan: -->
+                    <span id="errorMsgMultiple" class="error--txt-several">
+                    </span>
+
+                    <span class="container__btn">
+                      <button class="btn--primary btn--form" type="submit">
+                        Skicka
+                      </button>
+                      <button
+                        id="redirectSeminar"
+                        class="btn--secondary btn--form"
+                        type="button"
+                      >
+                        Läs mer
+                      </button>
+                    </span>
+                  </form>
+
+                  <div class="form__span-details">
+                    <p class="detail">Intresseanmälan är inte bindande.</p>
+                    <p class="detail">
+                      Vi samlar in telefonnummer och e-postadresser från
+                      formulär för att effektivt kommunicera med våra kudner och
+                      följer strikt GDPR för att säkerställa deras integritet
+                      och dataskydd.
+                    </p>
+                  </div>
+                </div>
+      
+      `;
+      break;
     default:
       modalContent.innerHTML = `<div class="modal" id="modalDiv">
       <div class="modal--error">
